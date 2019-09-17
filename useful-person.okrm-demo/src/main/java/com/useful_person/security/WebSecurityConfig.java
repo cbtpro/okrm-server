6b6e0e4ef617328c61013e2ccf8637468ffedca1
chenbitao <cbtpro@gmail.com>
2019-09-17 17:01:12 +0800

    删除安全配置的代码

--- useful-person.okrm-demo/src/main/java/com/useful_person/security/WebSecurityConfig.java
+++ useful-person.okrm-demo/src/main/java/com/useful_person/security/WebSecurityConfig.java
@@ -1,13 +1,14 @@
-package com.useful_person.security;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+//package com.useful_person.security;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
 //@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-	}
-}
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.csrf().disable();
+//	}
+//}
