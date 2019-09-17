35dc4a46934817cc5e5f42576b5eaba075064564
chenbitao <cbtpro@gmail.com>
2019-09-17 17:01:45 +0800

    将/目录跳转改成返回hello

--- useful-person.okrm-demo/src/main/java/com/useful_person/web/controller/WebRouter.java
+++ useful-person.okrm-demo/src/main/java/com/useful_person/web/controller/WebRouter.java
@@ -3,11 +3,13 @@ package com.useful_person.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebRouter {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
 	public String home() {
 		return "hello";
 	}
