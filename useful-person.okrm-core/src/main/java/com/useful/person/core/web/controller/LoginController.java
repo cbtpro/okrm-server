fa50890e1e4bf30157f0de5a0fa17cdba59f0aa6
陈碧滔 <cbtpro@163.com>
2020-01-21 16:13:02 +0800

    增加手机号注册

--- useful-person.okrm-core/src/main/java/com/useful/person/core/web/controller/LoginController.java
+++ useful-person.okrm-core/src/main/java/com/useful/person/core/web/controller/LoginController.java
@@ -8,23 +8,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.hashids.Hashids;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.useful.person.core.authentication.services.IUserService;
 import com.useful.person.core.domain.UserInfo;
 import com.useful.person.core.domain.UserInfo.UserInfoMobileSignupView;
-import com.useful.person.core.authentication.services.IUserService;
 import com.useful.person.core.properties.AppConstants;
 import com.useful.person.core.properties.SecurityConstants;
+import com.useful.person.core.utils.ShortID;
 
 /**
  * 
@@ -56,7 +56,7 @@ public class LoginController {
 	 */
 	@JsonView(UserInfo.UserInfoDetailView.class)
 	@PostMapping(SecurityConstants.DEFAULT_SIGN_UP_URL)
-	public Map<String, Object> createUser(HttpServletRequest request, HttpServletResponse response, @RequestParam(required = false) String type, @Valid UserInfo user, BindingResult errors) throws Exception {
+	public Map<String, Object> createUser(HttpServletRequest request, HttpServletResponse response, @Valid UserInfo user, BindingResult errors) throws Exception {
 		Map<String, Object> map = new HashMap<String, Object>(2);
 		if (errors.hasErrors()) {
 			Map<String, String> errorMap = new HashMap<String, String>(5);
@@ -68,26 +68,24 @@ public class LoginController {
 			map.put(AppConstants.DEFAULT_RETURN_MESSAGE, errorMap);
 			return map;
 		}
-		UserInfo newUser = null;
-		if (StringUtils.isEmpty(type) || SecurityConstants.DEFAULT_AUTH_TYPE_USERNAME.equals(type)) {
-			newUser = userService.register(user);
-		} else if(SecurityConstants.DEFAULT_AUTH_TYPE_MOBILE.equals(type)) {
-			user.setPassword(UUID.randomUUID().toString());
-			newUser = userService.registerByMobile(user);
-		}
-		if (newUser == null) {
-			map.put(AppConstants.DEFAULT_RETURN_MESSAGE, "用户注册失败");
-		} else {
-			map.put(AppConstants.DEFAULT_RETURN_MESSAGE, "用户注册成功");
-			map.put("user", newUser);
-		}
+		UserInfo newUser = userService.register(user);
+		map.put(AppConstants.DEFAULT_RETURN_MESSAGE, "用户注册成功！");
+		map.put("user", newUser);
 		return map;
 	}
 
 	@JsonView(UserInfoMobileSignupView.class)
 	@PostMapping(SecurityConstants.DEFAULT_SIGN_UP_MOBILE_URL)
-	public Map<String, Object> createUserByMobile() {
-		return null;
+	public Map<String, Object> createUserByMobile(UserInfo user) {
+		user.setPassword(UUID.randomUUID().toString());
+		String shortID = ShortID.getShortUuid();
+		user.setUsername(shortID);
+		user.setNickname(shortID);
+		UserInfo newUser = userService.registerByMobile(user);
+		Map<String, Object> map = new HashMap<>(2);
+		map.put(AppConstants.DEFAULT_RETURN_MESSAGE, "用户注册成功！");
+		map.put("user", newUser);
+		return map;
 	}
 	/**
 	 * 注销用户
