2dcea95468c5f7a2aba1aff61fa6057ace5b786f
chenbitao <cbtpro@gmail.com>
2020-04-03 20:41:16 +0800

    添加跨域配置

---
+++ useful-person.okrm-browser/src/main/java/com/useful/person/browser/CorsConfig.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.useful.person.browser;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author peter
+ *
+ */
+@Configurable
+public class CorsConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		WebMvcConfigurer.super.addCorsMappings(registry);
+		registry.addMapping("/**")
+			.allowedHeaders("*")
+			.allowedMethods("*")
+			.allowedOrigins("*");
+	}
+}
