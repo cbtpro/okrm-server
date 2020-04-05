b0bedd36f65ed9d668afae24f537b2ea170ab22d
陈碧滔 <peter.chen@useful-person.com>
2020-04-06 02:08:29 +0800

    重构代码、未登录时直接返回状态码401

--- useful-person.okrm-browser/src/main/java/com/useful/person/browser/BrowserCorsConfig.java
+++ useful-person.okrm-browser/src/main/java/com/useful/person/browser/BrowserCorsConfig.java
@@ -1,26 +1,27 @@
-/**
- * 
- */
-package com.useful.person.browser;
-
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * @author peter
- *
- */
-@Configurable
-public class BrowserCorsConfig implements WebMvcConfigurer {
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		WebMvcConfigurer.super.addCorsMappings(registry);
-		registry.addMapping("/**")
-			.allowedOrigins("*")
-			.allowedMethods("*")
-			.allowCredentials(true)
-			.maxAge(3600);
-	}
-}
+///**
+// * 
+// */
+//package com.useful.person.browser;
+//
+//import org.springframework.beans.factory.annotation.Configurable;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * @author peter
+// *
+// */
+//@Configurable
+//public class BrowserCorsConfig implements WebMvcConfigurer {
+//
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		WebMvcConfigurer.super.addCorsMappings(registry);
+//		registry.addMapping("/**")
+//			.allowedOrigins("*")
+//			.allowedMethods("*")
+//			.allowCredentials(true)
+//			.allowedHeaders("Access-Control-Allow-Credentials", "true")
+//			.maxAge(3600);
+//	}
+//}
