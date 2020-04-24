5bbb6c0ecd00873021ce96ba875fccb3b27e2d8d
chenbitao <cbtpro@gmail.com>
2020-04-24 16:47:28 +0800

    重构查询用户

--- useful-person.okrm-core/src/main/java/com/useful/person/core/web/admin/UserController.java
+++ useful-person.okrm-core/src/main/java/com/useful/person/core/web/admin/UserController.java
@@ -8,9 +8,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.useful.person.core.annotation.HasAdminRole;
@@ -18,6 +18,7 @@ import com.useful.person.core.constants.ReturnCode;
 import com.useful.person.core.domain.UserInfo;
 import com.useful.person.core.services.UserInfoService;
 import com.useful.person.core.vo.ResponseData;
+import com.useful.person.core.vo.UsersRequestVO;
 
 import io.swagger.annotations.ApiOperation;
 
@@ -28,22 +29,16 @@ public class UserController {
 	@Autowired
 	private UserInfoService userInfoService;
 
-	@GetMapping("/users")
+	@PostMapping("/users")
 	@ApiOperation("查询用户列表")
 	@HasAdminRole
-	public ResponseData<Page<UserInfo>> queryUsers(
-			@RequestParam(required = false) String username,
-			@RequestParam(required = false) String nickname,
-			@RequestParam(required = false) String mobile,
-			@RequestParam(required = false) String email,
-			@RequestParam(name = "registerTimeFrom", required = false) Long startTime,
-			@RequestParam(name = "registerTimeTo", required = false) Long endTime, Boolean enabled,
-			@RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "0") int page,
-			@RequestParam(defaultValue = "username") String sord) {
-		Sort sort = new Sort(Direction.ASC, sord);
-		Pageable pageable = PageRequest.of(page, size, sort);
-		Page<UserInfo> users = userInfoService.queryUsersPage(pageable, username, nickname, mobile, email,
-				startTime != null ? new Date(startTime) : null, endTime != null ? new Date(endTime) : null, enabled);
-		return new ResponseData<Page<UserInfo>>(ReturnCode.CORRECT.getCode(), "查询成功！", users);
+	public ResponseData<Page<UserInfo>> queryUsers(@RequestBody UsersRequestVO request) {
+		Sort sort = new Sort(request.getSortOrder().equals("ascend") ? Direction.ASC : Direction.DESC, request.getSortField());
+		Pageable pageable = PageRequest.of(request.getPage() - 1, request.getSize(), sort);
+		Long startTime = request.getRegisterTimeFrom();
+		Long endTime = request.getRegisterTimeTo();
+		Page<UserInfo> users = userInfoService.queryUsersPage(pageable, request.getUsername(), request.getNickname(), request.getMobile(), request.getEmail(),
+			startTime != null ? new Date(startTime) : null, endTime != null ? new Date(endTime) : null, request.getEnabled(), request.getRoles());
+		return new ResponseData<Page<UserInfo>>(ReturnCode.CORRECT.getCode(), null, users);
 	}
 }
