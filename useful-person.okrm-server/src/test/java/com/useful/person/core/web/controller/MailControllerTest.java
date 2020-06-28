0a0cf00552204013933f95d3ed19f650fe37b62e
chenbitao <cbtpro@gmail.com>
2020-06-28 11:58:18 +0800

    修改单元测试会启动多个测试实例的bug，保证注解一致即可

--- useful-person.okrm-server/src/test/java/com/useful/person/core/web/controller/MailControllerTest.java
+++ useful-person.okrm-server/src/test/java/com/useful/person/core/web/controller/MailControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,7 +20,7 @@ import org.springframework.web.context.WebApplicationContext;
  *
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MailControllerTest {
 
 	@Autowired
