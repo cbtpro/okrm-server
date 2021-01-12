2ba731ab0d79b10d28f5e39379d2d2925d8c0102
PeterChen <cbtpro@gmail.com>
2021-01-12 21:36:03 +0800

    添加加密解密脚本，增加加密解密文档，增加微信配置

---
+++ useful-person.okrm-core/src/main/java/com/useful/person/core/properties/TencentProperties.java
@@ -0,0 +1,15 @@
+package com.useful.person.core.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author peter
+ *
+ */
+public class TencentProperties {
+
+	@Getter
+	@Setter
+	private WeappProperties weapp = new WeappProperties();
+}
