1a0b6f3f69fe0ede869d71478a4c0cd07a81a1fd
PeterChen <cbtpro@gmail.com>
2021-05-05 17:54:45 +0800

    重构代码

--- useful-person.okrm-core/src/main/java/com/useful/person/core/web/admin/UserController.java
+++ useful-person.okrm-core/src/main/java/com/useful/person/core/web/admin/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,15 +18,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.useful.person.core.annotation.HasAdminRole;
 import com.useful.person.core.constants.ReturnCode;
 import com.useful.person.core.domain.UserInfo;
+import com.useful.person.core.properties.SecurityConstants;
 import com.useful.person.core.services.UserInfoService;
 import com.useful.person.core.vo.ResponseData;
 import com.useful.person.core.vo.UsersRequestVO;
 
 import io.swagger.annotations.ApiOperation;
 
+@PreAuthorize("hasRole('" + SecurityConstants.DEFAULT_ROLE_NAME_PREFIX + "ADMIN')")
 @RestController("managerUsersController")
 @RequestMapping("/admin")
 public class UserController {
@@ -35,7 +37,6 @@ public class UserController {
 
 	@PostMapping("/users")
 	@ApiOperation("查询用户列表")
-	@HasAdminRole
 	public ResponseData<Page<UserInfo>> queryUsers(@RequestBody UsersRequestVO request) {
 		Sort sort = Sort.by(request.getSortOrder().equals("ascend") ? Direction.ASC : Direction.DESC, request.getSortField());
 		Pageable pageable = PageRequest.of(request.getPage() - 1, request.getSize(), sort);
@@ -48,7 +49,6 @@ public class UserController {
 
 	@PostMapping("/admins")
 	@ApiOperation("查询拥有管理权限的用户")
-	@HasAdminRole
 	public ResponseData<Page<UserInfo>> queryUsersHasAdmin(@RequestBody UsersRequestVO request) {
 		Sort sort = Sort.by(request.getSortOrder().equals("ascend") ? Direction.ASC : Direction.DESC, request.getSortField());
 		Pageable pageable = PageRequest.of(request.getPage() - 1, request.getSize(), sort);
@@ -58,7 +58,6 @@ public class UserController {
 
 	@PutMapping("/admins")
 	@ApiOperation("批量将用户添加进管理员角色")
-	@HasAdminRole
 	public ResponseData<String> addUsersToAdmin(@RequestBody List<String> usernames) {
 		String msg = userInfoService.addUsernamesToAdmin(usernames);
 		return new ResponseData<String>(ReturnCode.CORRECT.getCode(), msg, null);
@@ -66,7 +65,6 @@ public class UserController {
 
 	@DeleteMapping("/admins")
 	@ApiOperation("批量删除用户的管理员角色")
-	@HasAdminRole
 	public ResponseData<String> removeUsersFromAdmin(@RequestParam(required = true) String uuid) {
 		String msg = userInfoService.removeUserFromAdmin(uuid);
 		return new ResponseData<String>(ReturnCode.CORRECT.getCode(), msg, null);
