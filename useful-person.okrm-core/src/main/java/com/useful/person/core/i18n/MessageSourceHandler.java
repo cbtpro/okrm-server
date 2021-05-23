65e6f5f35a858b7ce949252d3a26a3c3ffb0ecda
陈碧滔 <peter.chen@useful-person.com>
2021-05-23 17:11:02 +0800

    支持跟随浏览器请求使用国际化

--- useful-person.okrm-core/src/main/java/com/useful/person/core/i18n/MessageSourceHandler.java
+++ useful-person.okrm-core/src/main/java/com/useful/person/core/i18n/MessageSourceHandler.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
@@ -18,5 +19,10 @@ public class MessageSourceHandler {
 
     public String getMessage(String code) {
         return messageSource.getMessage(code, null, RequestContextUtils.getLocale(request));
+    }
+ 
+    public String getLocalMessage(String messageKey) {
+        String message = messageSource.getMessage(messageKey, null, LocaleContextHolder.getLocale());
+        return message;
     }
 }
