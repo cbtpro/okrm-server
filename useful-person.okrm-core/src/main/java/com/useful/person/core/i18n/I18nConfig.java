139a5cbc9465473ad4847c72993c60e1ccb27fc8
陈碧滔 <peter.chen@useful-person.com>
2021-05-23 17:11:02 +0800

    支持跟随浏览器请求使用国际化

--- useful-person.okrm-core/src/main/java/com/useful/person/core/i18n/I18nConfig.java
+++ useful-person.okrm-core/src/main/java/com/useful/person/core/i18n/I18nConfig.java
@@ -1,6 +1,7 @@
 package com.useful.person.core.i18n;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
@@ -10,9 +11,14 @@ public class I18nConfig {
     @Value(value = "${spring.messages.basename}")
     private String basename;
 
+    @Value(value = "${spring.messages.encoding}")
+    private String defaultEncoding;
+
+    @Bean(name = "messageSource")
     public ResourceBundleMessageSource getMessageResource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename(basename);
+        messageSource.setDefaultEncoding(defaultEncoding);
         return messageSource;
     }
 }
